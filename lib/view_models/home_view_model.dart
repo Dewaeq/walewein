89f68dc79efef7bf1e49841fd58ba6d18505a6b0
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2022-11-17 19:27:24 +0100

    calculate sum of graph price for multiple relations

--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -11,9 +11,9 @@ import 'package:walewein/shared/services/isar_service.dart';
 class HomeViewModel extends ViewModel<List<Graph>> {
   HomeViewModel(super.context);
 
-  late List<Graph> graphs;
-  late Map<Graph, bool> selectedGraphs;
-  late List<Price> prices;
+  List<Graph> graphs = [];
+  Map<Graph, bool> selectedGraphs = {};
+  List<Price> prices = [];
   bool isSelecting = false;
 
   late final StreamSubscription<List<Graph>> _graphSubscription;
@@ -26,12 +26,6 @@ class HomeViewModel extends ViewModel<List<Graph>> {
     final priceStream = isarService.listenPrices(false);
 
     prices = await isarService.getAllPrices();
-
-    if (prices.isEmpty) {
-      for (final entry in defaultPrices.entries) {
-        await isarService.savePrice(entry.value);
-      }
-    }
 
     _graphSubscription = graphSream.listen((event) => setState(event));
     _priceSubscription = priceStream.listen((event) => setPrices(event));
@@ -101,21 +95,24 @@ class HomeViewModel extends ViewModel<List<Graph>> {
   }
 
   int graphPrice(Graph graph) {
-    final relation = graph.relations.first;
-
-    if (relation.nodes.length < 2) return 0;
+    int result = 0;
 
     final now = DateTime.now();
-
     final currentVal = now.millisecondsSinceEpoch / millisInDay;
-    final currentUsage = GraphService.interpolate(relation.nodes, currentVal);
-
     final startVal =
         DateTime(now.year, now.month, 1).millisecondsSinceEpoch / millisInDay;
-    final startUsage = GraphService.interpolate(relation.nodes, startVal);
-
     final price = prices.firstWhere((e) => e.graphType == graph.graphType);
 
-    return ((currentUsage - startUsage) * price.price).round();
+    for (final relation in graph.relations) {
+      if (relation.nodes.length < 2) continue;
+
+      final currentUsage = GraphService.interpolate(relation.nodes, currentVal);
+
+      final startUsage = GraphService.interpolate(relation.nodes, startVal);
+
+      result += ((currentUsage - startUsage) * price.price).round();
+    }
+
+    return result;
   }
 }
