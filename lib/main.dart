9f3fe7f94ca04ad0529bc7042300bdaae89d94e8
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2022-11-17 09:49:46 +0100

    transparent background for android system navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -11,8 +12,20 @@ void main() async {
 
   await initializeDateFormatting("nl_BE", null);
   Intl.defaultLocale = "nl_BE";
+  
+  setSystemNavBarColor();
 
   runApp(const MyApp());
+}
+
+void setSystemNavBarColor() {
+  if (!Platform.isAndroid) return;
+  
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
